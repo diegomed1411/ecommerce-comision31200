f5747ffd2a3676de3da282ce5d43514cd74985a5
diegomed1411 <37563670+diegomed1411@users.noreply.github.com>
2022-05-25 07:56:32 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
