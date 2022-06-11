df5fa356cd8df4054bf6c846b1b87c695a421dc7
diegomed1411 <37563670+diegomed1411@users.noreply.github.com>
2022-06-11 20:29:27 -0300

    desafio montaje api

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import ItemDetail from '../ItemDetail/ItemDetail'
+import React from "react";
+import { useEffect, useState } from "react";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import getFetch from "../../utils/getFetch";
 
-const ItemDetailContainer = () => {
+const ItemDetailContainer = (item) => {
+  const [producto, setProducto] = useState([]);
+
+  useEffect(() => {
+    return new Promise(resolve)
+      .then((resp) => setProducto(resp))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
-      <ItemDetail />
+      <ItemDetail
+        key={producto.id}
+        id={producto.id}
+        title={producto.title}
+        description={producto.description}
+        shortDescription={producto.shortDescription}
+        price={producto.price}
+        pictureUrl={producto.productoUrl}
+      /> 
     </>
-  )
-}
+  );
+};
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
